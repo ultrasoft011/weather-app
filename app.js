b94b4ae2f309efce1e2d86bf1c2ebbf3eacf4579
ultrasoft011 <ultrasoft.011@gmail.com>
2021-09-13 13:22:59 -0500

    refactoring geocode function with callbacks

--- app.js
+++ app.js
@@ -27,25 +27,55 @@ const url =
 //
 // 1. Fire off a new request to the URL explored in browser
 
-const url2 =
-  "https://api.mapbox.com/geocoding/v5/mapbox.places/LosAngeles.json?access_token=pk.eyJ1IjoidWx0cmFzb2Z0IiwiYSI6ImNrbnM1cHkzeTAzODgycXMxemdpN3h6YzYifQ.k87LpRwQe8htBBBoDtCcOA";
+// const url2 =
+//   "https://api.mapbox.com/geocoding/v5/mapbox.places/LosAngeles.json?access_token=pk.eyJ1IjoidWx0cmFzb2Z0IiwiYSI6ImNrbnM1cHkzeTAzODgycXMxemdpN3h6YzYifQ.k87LpRwQe8htBBBoDtCcOA";
 
-// 2. Have the request module parse it as JSON
-// 3. Print both the latitude and longitude to the terminal
-request({ url: url2, json: true }, (error, response) => {
-  // Low-level error
-  if (error) {
-    console.log("Unable to connect to Geocoding");
-  } else if (response.body.features.length === 0) {
-    console.log(
-      `Unable to find the location on Geocoding, query ${response.body.query[0]} not valid`
-    );
-  } else {
-    const data2 = response.body;
-    const longitude = data2.features[0].center[0];
-    const latitud = data2.features[0].center[1];
-    console.log(
-      `The longitude of los Angeles is ${longitude} and the latitud is ${latitud}`
-    );
-  }
+// // 2. Have the request module parse it as JSON
+// // 3. Print both the latitude and longitude to the terminal
+// request({ url: url2, json: true }, (error, response) => {
+//   // Low-level error
+//   if (error) {
+//     console.log("Unable to connect to Geocoding");
+//   } else if (response.body.features.length === 0) {
+//     console.log(
+//       `Unable to find the location on Geocoding, query ${response.body.query[0]} not valid`
+//     );
+//   } else {
+//     const data2 = response.body;
+//     const longitude = data2.features[0].center[0];
+//     const latitud = data2.features[0].center[1];
+//     console.log(
+//       `The longitude of los Angeles is ${longitude} and the latitud is ${latitud}`
+//     );
+//   }
+// });
+
+// Refactoring the geocode program with callbacks
+//
+// Geocode function
+const geocode = (address, callback) => {
+  const url =
+    "https://api.mapbox.com/geocoding/v5/mapbox.places/" +
+    encodeURIComponent(address) +
+    ".json?access_token=pk.eyJ1IjoidWx0cmFzb2Z0IiwiYSI6ImNrbnM1cHkzeTAzODgycXMxemdpN3h6YzYifQ.k87LpRwQe8htBBBoDtCcOA";
+
+  request({ url: url, json: true }, (error, response) => {
+    if (error) {
+      callback("Unable to connect to location services!", undefined);
+    } else if (response.body.features.length === 0) {
+      callback("Unable to find the location on Geocoding", undefined);
+    } else {
+      callback(undefined, {
+        latitude: response.body.features[0].center[0],
+        latitud: response.body.features[0].center[1],
+        place: response.body.features[0].place_name,
+      });
+    }
+  });
+};
+
+// The caller: geocode
+geocode("Medellin", (error, data) => {
+  console.log("Error", error);
+  console.log("Data", data);
 });
