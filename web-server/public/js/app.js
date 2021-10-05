6116ce2263d3786e03e7c4c55999c5202dd63859
ultrasoft011 <ultrasoft.011@gmail.com>
2021-10-05 16:29:17 -0500

    Form and input address data

--- web-server/public/js/app.js
+++ web-server/public/js/app.js
@@ -1,24 +1,28 @@
 // const { response } = require("express");
-
-console.log("Ultra");
-
 // Template engine to render dynamic web pages using Express
 // Handlebars - create code we can reuse across pages npm (handlebars package) hbs library
 
-fetch("http://puzzle.mead.io/puzzle").then((response) => {
-  response.json().then((data) => {
-    console.log(data);
-  });
-});
+// fetch("http://puzzle.mead.io/puzzle").then((response) => {
+//   response.json().then((data) => {
+//     console.log(data);
+//   });
+// });
 
+const weatherForm = document.querySelector("form");
+const search = document.querySelector("input");
 
-// Fetch the data from the url
-fetch("http://localhost:3000/weather?address=Medellin").then((response) => {
-  response.json().then((data) => {
-    if (data.error) {
-      console.log(data.error);
-    } else {
-      console.log(data);
-    }
+weatherForm.addEventListener("submit", function (e) {
+  e.preventDefault(); // Prevent the default behavior, refresh the browser.
+  const location = search.value;
+
+  // Fetch the data from the url
+  fetch("http://localhost:3000/weather?address=" + location).then((response) => {
+    response.json().then((data) => {
+      if (data.error) {
+        console.log(data.error);
+      } else {
+        console.log(data);
+      }
+    });
   });
 });
